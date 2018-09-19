55da3d7d2ad02d8151ef3e07c69f0a69653af99a
Evan Miller <emiller@cloverhound.com>
2018-09-19 11:28:00 -0600

    Add redux

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Hello } from './components/Hello';
+import { Provider } from "react-redux";
+import store from "./store";
 declare let module: any
 
-ReactDOM.render(<Hello compiler="Typescript" framework="React..." bundler="Webpack" />, 
+ReactDOM.render(<Provider store={store}><Hello compiler="Typescript" framework="React..." bundler="Webpack"/></Provider>, 
 document.getElementById('root'));
 
 if (module.hot) {
---
+++ src/app/store/index.ts
@@ -0,0 +1,7 @@
+// src/js/store/index.js
+import {
+  createStore
+} from "redux";
+import rootReducer from "../reducers";
+const store = createStore(rootReducer);
+export default store;
