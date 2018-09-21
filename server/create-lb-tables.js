45f0057f8d6effb4d286cb14c23a528bad7c99ec
Evan Miller <emiller@cloverhound.com>
2018-09-21 17:07:26 -0600

    >Update AccessToken table id column so we can add Users. > Resource 'https://github.com/strongloop/loopback-connector-mssql/issues/53' > In order to fix the table, youll need to run this king of query > ALTER TABLE AccessToken DROP CONSTRAINT DF__AccessToken__id__3B40CD36; ALTER TABLE AccessToken DROP CONSTRAINT PK__AccessTo__3213E83F19E89FBD; ALTER TABLE AccessToken DROP COLUMN id ALTER TABLE AccessToken ADD id nvarchar(500);

--- server/create-lb-tables.js
+++ server/create-lb-tables.js
@@ -1,9 +1,30 @@
 var server = require('./server');
 var ds = server.dataSources.db;
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'Schedule'];
+var lbTables = ['AccessToken', 'ACL', 'RoleMapping', 'Role', 'Queue', 'Schedule', 'Holiday'];
+// var lbTables = ['Schedule', 'Holiday'];
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
   ds.disconnect();
 });
 
+// ds.isActual(lbTables, function (er, actual) {
+//   console.log('actual', actual);
+
+//   if (actual) {
+//     ds.autoupdate(lbTables, function (er, result) {
+//       console.log('result', result);
+
+//       if (er) throw er;
+//       console.log('Loopback tables [' + lbTables + '], have the result of [' + result + '] in ', ds.adapter.name);
+//       ds.disconnect();
+//     });
+//   }
+// });
+
+
+// ds.autoupdate(lbTables, function (er, result) {
+//   ds.discoverModelProperties('Schedule', function (err, props) {
+//     console.log(props);
+//   });
+// });
