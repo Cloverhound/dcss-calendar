226934b554389909b7afdc53561c52fcbdd1b8fc
Evan Miller <emiller@cloverhound.com>
2018-11-30 16:31:52 -0700

    Update folder breakouts

--- client_src/src/app/components/CalendarTable/CalendarTable.tsx
+++ client_src/src/app/components/CalendarTable/CalendarTable.tsx
@@ -12,8 +12,8 @@ import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 
-import CalendarTableHead from './CalendarTableHead/CalendarTableHead';
-import CalendarTableToolbar from './CalendarTableToolbar/CalendarTableToolBar';
+import CalendarTableHead from '../CalendarTableHead/CalendarTableHead';
+import CalendarTableToolbar from '../CalendarTableToolbar/CalendarTableToolBar';
 
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom';
--- client_src/src/app/components/Holidays/EditHolidayList.tsx
+++ client_src/src/app/components/Holidays/EditHolidayList.tsx
@@ -7,7 +7,7 @@ import AddIcon from '@material-ui/icons/Add';
 import green from '@material-ui/core/colors/green';
 import { connect } from 'react-redux';
 import TextField from '@material-ui/core/TextField';
-import CalendarSnackbar  from '../CalendarSnackbar/CalendarSnackBar';
+import CalendarSnackbar  from '../CalendarSnackbar/CalendarSnackbar';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import {
   BrowserRouter as Router,
