8301fc68d6b815759d43e712a70a237f5615abf3
Evan Miller <emiller@cloverhound.com>
2018-12-11 16:53:21 -0700

    Add createPrompt on successful s3 upload

--- common/models/prompt.js
+++ common/models/prompt.js
@@ -9,33 +9,41 @@ AWS.config.update({
 
 var s3 = new AWS.S3();
 
-module.exports = function(Prompt) {
+module.exports = function (Prompt) {
   s3Upload(Prompt)
-  
+
   Prompt.remoteMethod(
     'upload', {
-      http: {path: '/upload', verb: 'post'},
-      accepts: [
-        {
-        arg: 'req', type: 'object', http: function (ctx) {
-            return ctx.req;
+      http: {
+        path: '/upload',
+        verb: 'post'
+      },
+      accepts: [{
+        arg: 'req',
+        type: 'object',
+        http: function (ctx) {
+          return ctx.req;
         }
-        }],
-      returns: {arg: 'res', type: 'string'},
+      }],
+      returns: {
+        arg: 'res',
+        type: 'string'
+      },
     }
   )
 };
 
 function s3Upload(Prompt) {
-  Prompt.upload = function(promptFile, cb) {
+  // console.log("PROMPT : ", Object.getOwnPropertyNames(Prompt))
+  Prompt.upload = function (promptFile, cb) {
     var response;
     var params = {
       Bucket: process.env.AWS_BUCKET_NAME,
-      Key : promptFile.files[0].originalname,
-      Body : promptFile.files[0].buffer
+      Key: promptFile.files[0].originalname,
+      Body: promptFile.files[0].buffer
     };
 
-    s3.upload(params, function (err, data) {
+    s3.upload(params, async function (err, data) {
       //handle error
       if (err) {
         console.log("Error", err);
@@ -43,10 +51,33 @@ function s3Upload(Prompt) {
       }
       //success
       if (data) {
-        console.log("Uploaded in:", data.Location);
-        response = data.Location
+        let newPrompt = await createPrompt(Prompt, data.Location)
+        response = newPrompt
       }
       cb(null, response)
     });
   }
+}
+
+async function createPrompt(Prompt, url) {
+  let newPrompt = await new Promise(function (resolve, reject) {
+    Prompt.create({
+      name: Date.now(),
+      language: "english",
+      type: "office directions",
+      url,
+      queueId: 1
+    }, function (createPromptErr, createdPrompt) {
+
+      if (createPromptErr) {
+        return createPromptErr
+      }
+
+      if (createdPrompt !== 'FAILED') {
+        resolve(createdPrompt)
+        return createdPrompt
+      }
+    })
+  })
+  return newPrompt
 }
